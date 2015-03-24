c21982e11f02b8122ec86b255d3f378edbf39ab5
Ian McDowell <igmcdowell@gmail.com>
2015-03-24 13:51:15 -0700

    Add neorj setup instructions and gulp taks

--- README.md
+++ README.md
@@ -4,6 +4,10 @@
 - Install node through nvm. See https://github.com/creationix/nvm
 - Install gulp
 `npm install --global gulp`
+- install neo4j
+`brew install neo4j`
+- install JDK7 (neo4j dependency)
+http://www.oracle.com/technetwork/java/javase/downloads/jdk7-downloads-1880260.html
 
 ### Install Project Dependencies
 `npm install`
---
+++ gulp/tasks/start-db.js
@@ -0,0 +1,13 @@
+var gulp           = require('gulp')
+var exec = require('child_process').exec;
+
+gulp.task('start-db', function(callback) {
+var child = exec('neo4j start',
+  function (error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    }
+  });
+})
