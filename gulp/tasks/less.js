d3ac4d7e682f6702b62c4a8457f5977868c5a73e
Ian McDowell <igmcdowell@gmail.com>
2015-05-06 15:18:18 -0700

    Autoprefix our less

--- gulp/tasks/less.js
+++ gulp/tasks/less.js
@@ -2,11 +2,16 @@ var less = require('gulp-less');
 var path = require('path');
 var config  = require('../config').less
 var gulp     = require('gulp')
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('less', function () {
   return gulp.src(config.src)
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
     }))
     .pipe(gulp.dest(config.dest));
 });
