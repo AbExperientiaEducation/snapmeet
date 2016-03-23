9a1a625497eee31944936ed52777f13cbddceeb5
Ian McDowell <igmcdowell@gmail.com>
2016-03-22 18:28:16 -0700

    Remove remaining reference to gulp-watch plugin and replace with native gulp.watch

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,7 +3,6 @@
 */
 
 var gulp     = require('gulp')
-var watch = require('gulp-watch')
 var config   = require('../config')
 
 gulp.task('watch', ['watchify'], function(callback) {
@@ -11,13 +10,15 @@ gulp.task('watch', ['watchify'], function(callback) {
     gulp.run('sass')
   })
   gulp.watch(config.less.watch, function(){
-    gulp.run('less') 
+    gulp.run('less')
   })
-  watch(config.revReplace.watch, {events: ['add', 'change']}, function() {
+  gulp.watch(config.revReplace.watch, function() {
     gulp.run('rev-replace')
   })
+  
   // gulp.watch(config.revReplace.watch, ['rev-replace'])
   // gulp.watch(config.images.src, ['images'])
   // gulp.watch(config.markup.src, ['markup'])
   // Watchify will watch and recompile our JS, so no need to gulp.watch it
 })
+
